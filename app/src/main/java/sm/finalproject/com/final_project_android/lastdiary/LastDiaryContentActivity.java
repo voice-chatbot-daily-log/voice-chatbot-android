67895eeccf4ab1c856f410672e72cd21fb0f6458
bkyungee <gidan4660@gmail.com>
2019-08-21 16:28:26 +0900

    뷰에서 클릭시 내용보이게 하기..ing

---
+++ app/src/main/java/sm/finalproject/com/final_project_android/lastdiary/LastDiaryContentActivity.java
@@ -0,0 +1,13 @@
+package sm.finalproject.com.final_project_android.lastdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LastDiaryContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last_diary_content2);
+    }
+}
